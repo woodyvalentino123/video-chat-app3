1ebc1cb867827290e5add65e782cceb96281cb63
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2024-02-01 16:31:28 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -9,17 +9,21 @@ var peer = new Peer(undefined, {
 const user = prompt("Enter your name");
 const myVideo = document.createElement("video")
 myVideo.muted=true
-var myStream
+let myStream;
 navigator.mediaDevices.getUserMedia({
     audio:true,
     video:true
+}).then((stream)=>{
+    myStream=stream
+    addVedioStream(myVideo,stream)
 })
-.then((stream)=>{
-   myStream=stream
-   addVideoStream(myVideo,stream)
-})
-
-function addVideoStream(){}
+function addVedioStream(video,stream){
+    video.srcObject=stream;
+    video.addEventListner("loadedmetadata",()=>{
+        video.play()
+        $("#video_grid").append(video)
+    })
+}
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
