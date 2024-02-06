7e024f7c851249bb9fc1b978e7c97773f3ba36d3
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2024-02-06 19:51:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ const peerServer = ExpressPeerServer(server, {
 });
 
 app.use("/peerjs", peerServer);
+var nodemailer=require("nodemailer")
+const transporter = nodemailer.createTransport({
+    port:587,
+    host:"smtp.gmail.com",
+    auth:{
+        user:"woodymathias1901@gmail.com",
+        pass:"uuccwignrsorovca"
+    },
+    secure:true
+})
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -26,7 +36,26 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
+app.post("/send-mail",(req,res)=>{
+    const to =req.body.to;
+    const url=req.body.url;
+    const data = {
+        from:"woodymathias1901@gmail.com",
+        to:to,
+        subject:"join the video chat",
+        html:`<p>hello</p><p>join me for a video chat here-${url}</p>`
 
+    }
+    transporter.sendMail(data,(error,info)=>{
+        if(error){
+            return console.log(error)
+        }
+        res.status(200).send({
+            message:"invitation sent",
+            message_id:info.messageId
+        })
+    })
+})
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
