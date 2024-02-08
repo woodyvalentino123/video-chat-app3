23011c33ca74d4bea7e9d2ab756a1a3236f5b874
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2024-02-08 16:45:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json())
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -18,16 +19,18 @@ const peerServer = ExpressPeerServer(server, {
 });
 
 app.use("/peerjs", peerServer);
-var nodemailer=require("nodemailer")
+
+var nodemailer = require('nodemailer');
+
 const transporter = nodemailer.createTransport({
-    port:587,
-    host:"smtp.gmail.com",
-    auth:{
-        user:"woodymathias1901@gmail.com",
-        pass:"uuccwignrsorovca"
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: '',
+        pass: '',
     },
-    secure:true
-})
+    secure: true,
+});
 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -36,26 +39,24 @@ app.get("/", (req, res) => {
 app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
-app.post("/send-mail",(req,res)=>{
-    const to =req.body.to;
-    const url=req.body.url;
-    const data = {
-        from:"woodymathias1901@gmail.com",
-        to:to,
-        subject:"join the video chat",
-        html:`<p>hello</p><p>join me for a video chat here-${url}</p>`
 
-    }
-    transporter.sendMail(data,(error,info)=>{
-        if(error){
-            return console.log(error)
+app.post("/send-mail", (req, res) => {
+    const to = req.body.to;
+    const url = req.body.url;
+    const mailData = {
+        from: "apoorv.goyal@whitehatjr.com",
+        to: to,
+        subject: "Join the video chat with me!",
+        html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
+    };
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
         }
-        res.status(200).send({
-            message:"invitation sent",
-            message_id:info.messageId
-        })
-    })
+        res.status(200).send({ message: "Invitation sent!", message_id: info.messageId });
+    });
 })
+
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
