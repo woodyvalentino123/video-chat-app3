061dd4a3d180c9ebe8187e38dbfe26cf55802780
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2024-02-08 16:24:21 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -106,7 +106,26 @@ $(function () {
             $("#stop_video").html(html)
         }
     })
-
+$("#invite_button").click(function(){
+    const destination=prompt("enter the email address")
+    let data = {
+        url:window.location.href,
+        to:destination
+    }
+    $.ajax({
+        url:"/send-mail",
+        type:"post",
+        data:JSON.stringify(data),
+        dataType:"json",
+        contentType:"application/json",
+        success:function(result){
+            alert("invite sent")
+        },
+        error:function(result){
+            console.log(result.responseJSON)
+        }
+    })
+})
 })
 
 peer.on("open", (id) => {
